1096839a3b4827f1e4ad32f5af02e8a1209d0f9e
Riosberg Dutra <riosbergduttra@gmail.com>
2024-02-15 04:44:14 -0300

    table de usuario

---
+++ usuario/src/main/resources/db/migration/V1__Create-table-usuario.sql
@@ -0,0 +1,10 @@
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome_completo VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    endereco VARCHAR(255),
+    numero_telefone VARCHAR(20),
+    data_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    role VARCHAR(50) DEFAULT 'user'
+);
