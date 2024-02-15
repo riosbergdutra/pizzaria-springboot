ec2972165199c4240cfaf104552b5928140e8ff5
Riosberg Dutra <riosbergduttra@gmail.com>
2024-02-15 04:51:22 -0300

    service para usuario

---
+++ usuario/src/main/java/br/com/usuario/usuario/services/impl/UsuarioServiceImpl.java
@@ -0,0 +1,69 @@
+package br.com.usuario.usuario.services.impl;
+
+import br.com.usuario.usuario.dtos.UsuarioDto;
+import br.com.usuario.usuario.models.Usuario;
+import br.com.usuario.usuario.repositories.UsuarioRepository;
+import br.com.usuario.usuario.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UsuarioDto createUserService(UsuarioDto usuarioDto) {
+        Usuario usuarioJaExiste = usuarioRepository.findByEmail(usuarioDto.email());
+        if (usuarioJaExiste != null) {
+            throw new RuntimeException("Usuário já existe!");
+        }
+        String senhaHash = passwordEncoder.encode(usuarioDto.senha());
+        Usuario usuario = new Usuario(null, usuarioDto.nome_completo(), usuarioDto.email(), senhaHash,
+                usuarioDto.endereco(), usuarioDto.nome_completo(), null, null);
+        Usuario novoUsuario = usuarioRepository.save(usuario);
+        return new UsuarioDto(novoUsuario);
+    }
+
+    @Override
+    public UsuarioDto findUserByIdService(Long id) {
+        Optional<Usuario> optionalUsuario = usuarioRepository.findById(id);
+        return optionalUsuario.map(UsuarioDto::new).orElse(null);
+    }
+
+    @Override
+    public List<UsuarioDto> findAllUsersService() {
+        List<Usuario> usuarios = usuarioRepository.findAll();
+        return usuarios.stream().map(UsuarioDto::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public UsuarioDto updateUserService(Long id, UsuarioDto usuarioDto) {
+        Optional<Usuario> optionalUsuario = usuarioRepository.findById(id);
+        if (optionalUsuario.isPresent()) {
+            Usuario usuario = optionalUsuario.get();
+            usuario.setNome_completo(usuarioDto.nome_completo());
+            usuario.setEmail(usuarioDto.email());
+            usuario.setEndereco(usuarioDto.endereco());
+            usuario.setNumero_telefone(usuarioDto.numero_telefone());
+            usuarioRepository.save(usuario);
+            return new UsuarioDto(usuario);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void removeUserService(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+}
