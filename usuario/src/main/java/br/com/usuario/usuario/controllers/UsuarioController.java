6b005fa4dc624312c4661fe2d1182a40f5607ad5
Riosberg Dutra <riosbergduttra@gmail.com>
2024-02-16 08:53:43 -0300

    mudanca no controller para cache em dynamodb

--- usuario/src/main/java/br/com/usuario/usuario/controllers/UsuarioController.java
+++ usuario/src/main/java/br/com/usuario/usuario/controllers/UsuarioController.java
@@ -1,31 +1,38 @@
 package br.com.usuario.usuario.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import br.com.usuario.usuario.dtos.UsuarioDto;
+import br.com.usuario.usuario.services.UsuarioCacheService;
 import br.com.usuario.usuario.services.UsuarioService;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
+@EnableCaching
 public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
 
+    @Autowired
+    private UsuarioCacheService usuarioCacheService;
+
     @PostMapping
     public ResponseEntity<UsuarioDto> createUserController(@RequestBody UsuarioDto usuarioDto) {
         UsuarioDto novoUsuario = usuarioService.createUserService(usuarioDto);
+        usuarioCacheService.cacheUsuario(novoUsuario.getId().toString(), novoUsuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UsuarioDto> findUserByIdController(@PathVariable Long id) {
-        UsuarioDto usuarioDto = usuarioService.findUserByIdService(id);
+        UsuarioDto usuarioDto = usuarioCacheService.getUsuarioFromCache(id.toString());
         if (usuarioDto != null) {
             return ResponseEntity.ok(usuarioDto);
         } else {
@@ -42,6 +49,7 @@ public class UsuarioController {
     @PutMapping("/{id}")
     public ResponseEntity<UsuarioDto> updateUserController(@PathVariable Long id, @RequestBody UsuarioDto usuarioDto) {
         UsuarioDto usuarioAtualizado = usuarioService.updateUserService(id, usuarioDto);
+        usuarioCacheService.cacheUsuario(id.toString(), usuarioAtualizado); 
         if (usuarioAtualizado != null) {
             return ResponseEntity.ok(usuarioAtualizado);
         } else {
@@ -52,6 +60,7 @@ public class UsuarioController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> removeUserController(@PathVariable Long id) {
         usuarioService.removeUserService(id);
+        usuarioCacheService.cacheUsuario(id.toString(), null); // Removendo do cache
         return ResponseEntity.noContent().build();
     }
 }
