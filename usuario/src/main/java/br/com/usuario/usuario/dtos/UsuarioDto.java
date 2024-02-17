3f77280f198e56f99e466a69c487dd63d06973d5
Riosberg Dutra <riosbergduttra@gmail.com>
2024-02-16 23:04:20 -0300

    metodos para json

--- usuario/src/main/java/br/com/usuario/usuario/dtos/UsuarioDto.java
+++ usuario/src/main/java/br/com/usuario/usuario/dtos/UsuarioDto.java
@@ -1,5 +1,8 @@
 package br.com.usuario.usuario.dtos;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import br.com.usuario.usuario.models.Usuario;
 
 public record UsuarioDto(
@@ -28,4 +31,28 @@ public record UsuarioDto(
    public String getEmail() {
       return email;
    }
+
+   
+   public static UsuarioDto fromJson(String json) {
+      if (json == null || json.isEmpty()) {
+          throw new IllegalArgumentException("A string JSON fornecida está vazia ou nula");
+      }
+      try {
+          ObjectMapper objectMapper = new ObjectMapper();
+          return objectMapper.readValue(json, UsuarioDto.class);
+      } catch (JsonProcessingException e) {
+          throw new IllegalArgumentException("Erro ao converter JSON para UsuarioDto", e);
+      }
+  }
+
+public String toJson() {
+   
+   try {
+      ObjectMapper objectMapper = new ObjectMapper();
+      return objectMapper.writeValueAsString(this);
+  } catch (JsonProcessingException e) {
+      throw new RuntimeException("Erro ao converter UsuarioDto para JSON", e);
+  }
+}
+   
 }
