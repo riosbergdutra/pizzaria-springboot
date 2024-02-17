3eeb2b2eef4e4dd8b37bd5404568137e19dd5221
Riosberg Dutra <riosbergduttra@gmail.com>
2024-02-17 03:07:05 -0300

    adicionando meus impl

--- usuario/src/main/java/br/com/usuario/usuario/services/UsuarioCacheService.java
+++ usuario/src/main/java/br/com/usuario/usuario/services/UsuarioCacheService.java
@@ -5,4 +5,6 @@ import br.com.usuario.usuario.dtos.UsuarioDto;
 public interface UsuarioCacheService {
     void cacheUsuario(String key, UsuarioDto usuarioDto);
     UsuarioDto getUsuarioFromCache(String key);
+    void removeUsuarioFromCache(String key);
+    void updateUsuarioInCache(String key, UsuarioDto usuarioDto);
 }
