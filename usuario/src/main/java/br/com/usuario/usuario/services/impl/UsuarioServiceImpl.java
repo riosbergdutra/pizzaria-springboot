1e7126239d53db690324b863360ffeff51b423c2
Riosberg Dutra <riosbergduttra@gmail.com>
2024-02-22 13:46:56 -0300

    enviando mensagem sobre o usuario

--- usuario/src/main/java/br/com/usuario/usuario/services/impl/UsuarioServiceImpl.java
+++ usuario/src/main/java/br/com/usuario/usuario/services/impl/UsuarioServiceImpl.java
@@ -12,6 +12,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import io.awspring.cloud.sqs.operations.SqsTemplate;
+
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
 
@@ -20,6 +22,9 @@ public class UsuarioServiceImpl implements UsuarioService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private SqsTemplate sqsTemplate; // Injeção do SqsTemplate
 
     @Override
     public UsuarioDto createUserService(UsuarioDto usuarioDto) {
@@ -31,6 +36,12 @@ public class UsuarioServiceImpl implements UsuarioService {
         Usuario usuario = new Usuario(null, usuarioDto.nome_completo(), usuarioDto.email(), senhaHash,
                 usuarioDto.endereco(), usuarioDto.nome_completo(), null, null);
         Usuario novoUsuario = usuarioRepository.save(usuario);
+
+        // Envio da mensagem para a fila SQS
+        String queueUrl = "http://localhost:4566/000000000000/minha-fila";
+        String messageBody = "Novo usuário cadastrado: " + usuarioDto.getEmail();
+        sqsTemplate.send(queueUrl, messageBody);
+
         return new UsuarioDto(novoUsuario);
     }
 
@@ -38,42 +49,62 @@ public class UsuarioServiceImpl implements UsuarioService {
     public UsuarioDto findUserByIdService(Long id) {
         @SuppressWarnings("null")
         Optional<Usuario> optionalUsuario = usuarioRepository.findById(id);
+        if (optionalUsuario.isPresent()) {
+            // Envio da mensagem para a fila SQS
+            String queueUrl = "http://localhost:4566/000000000000/minha-fila";
+            String messageBody = "Usuário encontrado pelo ID: " + id;
+            sqsTemplate.send(queueUrl, messageBody);
+        }
         return optionalUsuario.map(UsuarioDto::new).orElse(null);
     }
 
     @Override
     public List<UsuarioDto> findAllUsersService() {
         List<Usuario> usuarios = usuarioRepository.findAll();
+        // Envio da mensagem para a fila SQS
+        String queueUrl = "http://localhost:4566/000000000000/minha-fila";
+        String messageBody = "Consultando todos os usuários";
+        sqsTemplate.send(queueUrl, messageBody);
         return usuarios.stream().map(UsuarioDto::new).collect(Collectors.toList());
     }
 
     @Override
-public UsuarioDto updateUserService(Long id, UsuarioDto usuarioDto) {
-    @SuppressWarnings("null")
-    Optional<Usuario> optionalUsuario = usuarioRepository.findById(id);
-    if (optionalUsuario.isPresent()) {
-        Usuario usuario = optionalUsuario.get();
-        usuario.setNome_completo(usuarioDto.getNome_completo());
-        usuario.setEmail(usuarioDto.getEmail());
-        usuario.setEndereco(usuarioDto.getEndereco());
-        usuario.setNumero_telefone(usuarioDto.getNumero_telefone());
-        
-        if (!usuarioDto.getSenha().equals(usuario.getSenha())) {
-            String senhaHash = passwordEncoder.encode(usuarioDto.getSenha());
-            usuario.setSenha(senhaHash);
-        }
-        
-        usuarioRepository.save(usuario);
-        return new UsuarioDto(usuario);
-    } else {
-        return null;
-    }
-}
+    public UsuarioDto updateUserService(Long id, UsuarioDto usuarioDto) {
+        @SuppressWarnings("null")
+        Optional<Usuario> optionalUsuario = usuarioRepository.findById(id);
+        if (optionalUsuario.isPresent()) {
+            Usuario usuario = optionalUsuario.get();
+            usuario.setNome_completo(usuarioDto.getNome_completo());
+            usuario.setEmail(usuarioDto.getEmail());
+            usuario.setEndereco(usuarioDto.getEndereco());
+            usuario.setNumero_telefone(usuarioDto.getNumero_telefone());
 
+            if (!usuarioDto.getSenha().equals(usuario.getSenha())) {
+                String senhaHash = passwordEncoder.encode(usuarioDto.getSenha());
+                usuario.setSenha(senhaHash);
+            }
+
+            usuarioRepository.save(usuario);
+
+            // Envio da mensagem para a fila SQS
+            String queueUrl = "http://localhost:4566/000000000000/minha-fila";
+            String messageBody = "Usuário atualizado pelo ID: " + id;
+            sqsTemplate.send(queueUrl, messageBody);
+
+            return new UsuarioDto(usuario);
+        } else {
+            return null;
+        }
+    }
 
     @SuppressWarnings("null")
     @Override
     public void removeUserService(Long id) {
         usuarioRepository.deleteById(id);
+
+        // Envio da mensagem para a fila SQS
+        String queueUrl = "http://localhost:4566/000000000000/minha-fila";
+        String messageBody = "Usuário removido pelo ID: " + id;
+        sqsTemplate.send(queueUrl, messageBody);
     }
 }
