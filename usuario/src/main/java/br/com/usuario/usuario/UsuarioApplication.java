3430e510917aa07db63eab47dc94f369b261b8c5
Riosberg Dutra <riosbergduttra@gmail.com>
2024-02-22 13:46:28 -0300

    adicionando logica de envio de mensagem

--- usuario/src/main/java/br/com/usuario/usuario/UsuarioApplication.java
+++ usuario/src/main/java/br/com/usuario/usuario/UsuarioApplication.java
@@ -1,13 +1,25 @@
 package br.com.usuario.usuario;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.awspring.cloud.sqs.operations.SqsTemplate;
+
+
 @SpringBootApplication
-public class UsuarioApplication {
+public class UsuarioApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsuarioApplication.class, args);
 	}
-
+	@Autowired
+	private SqsTemplate sqsTemplate;
+	@Override
+	public void run(String... args) throws Exception {
+		String queueUrl = "http://localhost:4566/000000000000/minha-fila";
+        String messageBody = "mensagem";
+        sqsTemplate.send(queueUrl,messageBody);
+	}
 }
