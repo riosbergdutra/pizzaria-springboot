3b9a3c7047a279f62b56a64bcc37b2b7d4beb97e
Riosberg Dutra <riosbergduttra@gmail.com>
2024-02-23 15:19:43 -0300

    tabela produtos e properties

---
+++ e-commerce/src/main/resources/db/migration/V1__create-table-produtos.sql/V2__Create-table-produto.sql
@@ -0,0 +1,9 @@
+CREATE TABLE produto (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    preco_unitario DOUBLE NOT NULL,
+    imagem VARCHAR(255) NOT NULL,
+    codigo_barra INT NOT NULL UNIQUE,
+    categoria VARCHAR(255) NOT NULL
+);
