dbd6b98623f3bcb3345a54f2bbd677049968e159
Riosberg Dutra <riosbergduttra@gmail.com>
2024-02-26 14:45:47 -0300

    criando tabela para carrinho

--- carrinho/src/main/resources/db/migration/V1__create_table.sql
+++ carrinho/src/main/resources/db/migration/V1__create_table.sql
@@ -1,10 +1,10 @@
-CREATE TABLE pizzaria_carrinho (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    userId BIGINT,
-    productId BIGINT,
-    quantity INT,
+CREATE TABLE carrinho (
+    carrinho_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_Id BIGINT,
+    product_Id BIGINT,
+    quantidade INT,
     preco_total DECIMAL(10, 2),
     frete DECIMAL(10, 2),
-    FOREIGN KEY (userId) REFERENCES pizzaria_usuarios.usuarios(id),
-    FOREIGN KEY (productId) REFERENCES pizzaria.produto(id)
+    FOREIGN KEY (user_Id) REFERENCES pizzaria_usuarios.usuarios(user_Id),
+    FOREIGN KEY (product_Id) REFERENCES pizzaria_produtos.produto(product_Id)
 );
