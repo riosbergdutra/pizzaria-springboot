918a695fd2459a84d5fbd55a6dd4efa9e6ce1026
Riosberg Dutra <riosbergduttra@gmail.com>
2024-02-26 14:50:40 -0300

    dto de carrinho

---
+++ carrinho/src/main/java/msc/carrinho/carrinho/dtos/CarrinhoDto.java
@@ -0,0 +1,71 @@
+package msc.carrinho.carrinho.dtos;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import msc.carrinho.carrinho.model.Carrinho;
+
+public record CarrinhoDto(
+    Long carrinho_id,
+    Long user_Id,
+    Long product_Id,
+    int quantidade,
+    double preco_total,
+    double frete
+) {
+    public CarrinhoDto(Carrinho novoCarrinho) {
+        this(
+            novoCarrinho.getCarrinho_id(),
+            novoCarrinho.getUser_Id(),
+            novoCarrinho.getProduct_Id(),
+            novoCarrinho.getQuantidade(),
+            novoCarrinho.getPreco_total(),
+            novoCarrinho.getFrete()
+        );
+    }
+
+    public Long getCarrinho_id() {
+        return product_Id;
+    }
+    
+     public Long getUser_Id() {
+        return user_Id;
+    }
+
+     public Long getProduct_Id() {
+        return product_Id;
+    }
+
+     public int getQuantidade() {
+        return quantidade;
+    }
+
+     public double getPreco_total() {
+        return preco_total;
+    }
+
+     public double getFrete() {
+        return frete;
+    }
+
+      public static CarrinhoDto fromJson(String json) {
+        if (json == null || json.isEmpty()) {
+            throw new IllegalArgumentException("A string JSON fornecida está vazia ou nula");
+        }
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.readValue(json, CarrinhoDto.class);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("Erro ao converter JSON para ProdutoDto", e);
+        }
+    }
+
+    public String toJson() {  
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Erro ao converter ProdutoDto para JSON", e);
+        }
+    }
+}
