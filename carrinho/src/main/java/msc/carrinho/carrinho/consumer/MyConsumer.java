19dddb7c11fec6631e308b6ba4c68e62f6ceea7f
Riosberg Dutra <riosbergduttra@gmail.com>
2024-02-26 14:49:47 -0300

    consumer

---
+++ carrinho/src/main/java/msc/carrinho/carrinho/consumer/MyConsumer.java
@@ -0,0 +1,14 @@
+package msc.carrinho.carrinho.consumer;
+
+import org.springframework.stereotype.Component;
+
+import io.awspring.cloud.sqs.annotation.SqsListener;
+
+@Component
+public class MyConsumer {
+    
+    @SqsListener("minha-fila")
+    public void receiveMessage(String message) {
+            System.out.println(message);
+    }
+}
