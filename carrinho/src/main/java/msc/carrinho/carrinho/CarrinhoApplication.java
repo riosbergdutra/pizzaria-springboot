689d7fd1c467796c0ea99d39a7733398f17c5dee
Riosberg Dutra <riosbergduttra@gmail.com>
2024-02-26 14:45:13 -0300

    adicionando mensagem

--- carrinho/src/main/java/msc/carrinho/carrinho/CarrinhoApplication.java
+++ carrinho/src/main/java/msc/carrinho/carrinho/CarrinhoApplication.java
@@ -1,13 +1,26 @@
 package msc.carrinho.carrinho;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.awspring.cloud.sqs.operations.SqsTemplate;
+
 @SpringBootApplication
-public class CarrinhoApplication {
+public class CarrinhoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarrinhoApplication.class, args);
 	}
+	@Autowired
+	private SqsTemplate sqsTemplate;
+	@Override
+	public void run(String... args) throws Exception {
+		String queueUrl = "http://localhost:4566/000000000000/minha-fila";
+        String messageBody = "mensagem";
+        sqsTemplate.send(queueUrl,messageBody);
+	}
+
 
 }
