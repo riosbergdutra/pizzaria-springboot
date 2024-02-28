5c9c0c8b06deba30bf9fa68119cb32f1cae9d33f
Riosberg Dutra <riosbergduttra@gmail.com>
2024-02-28 17:06:33 -0300

    criando configuracao para dynamo

---
+++ pedido/src/main/java/msc/pedido/pedido/config/DynamodbConfig.java
@@ -0,0 +1,21 @@
+package msc.pedido.pedido.config;
+
+import java.net.URI;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+@Configuration
+public class DynamodbConfig {
+    
+    @Bean
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+        .endpointOverride(URI.create("http://localhost:4566"))
+        .region(Region.US_EAST_1)
+        .build();
+    }
+}
